1b7e3a94377b91db3947d94601718469d5041626
BlackVarmir <blackvarmir@gmail.com>
2025-05-27 22:18:09 +0300

    Resolved merge conflicts

--- src/pages/api/logScreenResolution.js
+++ src/pages/api/logScreenResolution.js
@@ -1,20 +1,9 @@
-<<<<<<< HEAD
 export default function handler(req, res) {
-    if (req.method === "POST") {
-      const { width, height } = req.body;
-      console.log(`Screen resolution: ${width}x${height}`);
-      res.status(200).json({ message: "Resolution logged" });
-    } else {
-      res.status(405).json({ message: "Only POST requests allowed" });
-    }
-=======
-export default function handler(req, res) {
-    if (req.method === "POST") {
-      const { width, height } = req.body;
-      console.log(`Screen resolution: ${width}x${height}`);
-      res.status(200).json({ message: "Resolution logged" });
-    } else {
-      res.status(405).json({ message: "Only POST requests allowed" });
-    }
->>>>>>> origin/main
+  if (req.method === "POST") {
+    const { width, height } = req.body;
+    console.log(`Screen resolution: ${width}x${height}`);
+    res.status(200).json({ message: "Resolution logged" });
+  } else {
+    res.status(405).json({ message: "Only POST requests allowed" });
   }
+}
